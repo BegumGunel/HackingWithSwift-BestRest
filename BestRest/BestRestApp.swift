1ce7daabfa5cebf763295bd9ad5d6a193779a7e4
Begum Gunel <begumgunel@gmail.com>
2022-02-07 10:29:02 +0300

    Initial Commit

---
+++ BestRest/BestRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BestRestApp.swift
+//  BestRest
+//
+//  Created by Begüm Günel on 7.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct BestRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
